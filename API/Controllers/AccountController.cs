974aeb5b3f56d36adc13209497eb7278b2593b5d
Lejla <lejla.basic@edu.fit.ba>
2023-08-27 01:10:14 +0200

    login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,11 +33,26 @@ namespace API.Controllers
 
             return user;
         }
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login (LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized();
+
+            using var hmac = new HMACSHA512(user.PaswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i = 0; i < computedHash.Length;i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+            return user;
+
+        }
         private async Task<bool> UserExists (string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
-    
     
 }
