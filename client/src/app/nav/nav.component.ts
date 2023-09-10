73e1105e63c791fa0b3fc5caf5be87f57e75d402
Lejla <lejla.basic@edu.fit.ba>
2023-09-10 19:21:57 +0200

    Routes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model:any={}
   currentUser$: Observable<User | null> = of(null)
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService, private router: Router) { }
 
   ngOnInit(): void {
     
@@ -20,15 +21,18 @@ export class NavComponent implements OnInit {
 
   login()
   {
-    this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-      },error =>{console.log(error);})
+    this.accountService.login(this.model).subscribe({
+      next:() =>
+        this.router.navigateByUrl('/members'),
+    error: error =>console.log(error)
       
-  }
+  })
+}
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
    
   }
 
