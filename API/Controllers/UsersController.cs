66a7c93b57c3df9d3749af1de890009caea51e9d
Lejla <lejla.basic@edu.fit.ba>
2023-09-14 16:47:02 +0200

    Edit profile and expanding the user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.Controllers;
+﻿using System.Security.Claims;
+using API.Controllers;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -34,5 +35,20 @@ public class UsersController : BaseApiController
     {
         return await _userRepository.GetMemberAsync(username);
 
+    }
+    [HttpPut]
+
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if(user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto,user);
+
+        if(await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,7 +9,7 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-members: Member[] = [];
+members$: Observable<Member[]> | undefined;
 
 constructor(private memberService: MembersService) {
 
@@ -16,12 +17,7 @@ constructor(private memberService: MembersService) {
 }
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-  loadMembers(){
-    this.memberService.getMembers().subscribe({
-      next: members =>this.members = members
-    })
+    this.members$ = this.memberService.getMembers();
   }
 
 }
