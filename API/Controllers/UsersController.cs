fe1981765764a3f5bf17775d5f1643809e310222
mauiwowieh <senaidaduran387@gmail.com>
2023-09-28 19:36:04 +0200

    Paginacija

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,8 @@
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -25,9 +27,10 @@ public class UsersController : BaseApiController
         _mapper = mapper;
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
-       var users = await _userRepository.GetMembersAsync();
+       var users = await _userRepository.GetMembersAsync(userParams);
+       Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
        return Ok(users);
     }
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,7 +10,12 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-members$: Observable<Member[]> | undefined;
+// members$: Observable<Member[]> | undefined;
+members:Member[] = [];
+
+pagination:Pagination | undefined;
+pageNumber=1;
+pageSize=3;
 
 constructor(private memberService: MembersService) {
 
@@ -17,7 +23,26 @@ constructor(private memberService: MembersService) {
 }
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
   }
 
+  loadMembers() {
+    //this.memberService.setUserParams(this.userParams);
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+
+
+      }})
+  }
+  pageChanged(event:any){
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
+
+  }
 }
