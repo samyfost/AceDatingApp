802237a946035b361d153be1455d46f0a8d55068
mauiwowieh <senaidaduran387@gmail.com>
2023-09-29 11:04:50 +0200

    Filtriranje korisnika po dobi i spolu

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -29,8 +29,17 @@ public class UsersController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
-       var users = await _userRepository.GetMembersAsync(userParams);
-       Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
+        var currentUser = await _userRepository.GetUserByUsernameAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+        userParams.CurrentUsername = currentUser.UserName;
+
+        if (string.IsNullOrEmpty(userParams.Gender))
+        {
+            userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+        }
+        var users = await _userRepository.GetMembersAsync(userParams);
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
+        
        return Ok(users);
     }
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParams';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -14,12 +17,19 @@ export class MemberListComponent implements OnInit{
 members:Member[] = [];
 
 pagination:Pagination | undefined;
-pageNumber=1;
-pageSize=3;
+userParams:UserParams | undefined;
+user:User | undefined;
+genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
-constructor(private memberService: MembersService) {
-
-  
+constructor(private memberService: MembersService, private accountService: AccountService) {
+  this.accountService.currentUser$.pipe(take(1)).subscribe({
+    next: user=>{
+      if(user){
+        this.userParams = new UserParams(user);
+        this.user = user;
+      }
+    }
+  })
 }
 
   ngOnInit(): void {
@@ -27,20 +37,27 @@ constructor(private memberService: MembersService) {
   }
 
   loadMembers() {
-    //this.memberService.setUserParams(this.userParams);
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    if(!this.userParams) return;
+
+    this.memberService.getMembers(this.userParams).subscribe({
       next: response => {
         if (response.result && response.pagination) {
           this.members = response.result;
           this.pagination = response.pagination;
         }
-
-
       }})
   }
+
+  resetFilters() {
+    if(this.user){
+      this.userParams = new UserParams(this.user);
+      this.loadMembers();
+    }
+  }
+
   pageChanged(event:any){
-    if(this.pageNumber !== event.page){
-      this.pageNumber = event.page;
+    if(this.userParams && this.userParams?.pageNumber !== event.page){
+      this.userParams.pageNumber = event.page;
       this.loadMembers();
     }
 
